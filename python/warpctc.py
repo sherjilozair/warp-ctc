44a24569d5937e65c9d89d6cf75ccb44ec82e463
Sherjil Ozair <sherjilozair@gmail.com>
2016-01-20 15:21:20 -0800

    Better interface

--- python/warpctc.py
+++ python/warpctc.py
@@ -18,30 +18,39 @@ libwarpctc.cpu_ctc.argtypes = [
         npct.ndpointer(dtype=np.float32, ndim=1),
         ctypes.c_int]
 
-def cpu_ctc(acts, labels, sizes):
+def cpu_ctc(acts, act_lens, labels, label_lens):
     """
     acts: 3-d numpy float array, same as c++ bindings
+    act_lens: 1-d int array of input length of each example
     labels: list of 1-d int array for each example in minibatch
-    sizes: 1-d int array of input size of each example
+    label_lens: 1-d int array of label length of each example
     """
+    # make sure correct types
     acts = np.array(acts, dtype=np.float32)
-    sizes = np.array(sizes, dtype=np.int32)
-    grads = np.zeros_like(acts, dtype=np.float32)
-    label_lengths = np.array([label.size for label in labels], dtype=np.int32)
-    flat_labels = np.concatenate(labels).astype(np.int32)
+    act_lens = np.array(act_lens, dtype=np.int32)
+    labels = np.array(labels, dtype=np.int32)
+    label_lens = np.array(label_lens, dtype=np.int32)
+
+    # C needs sizes
     alphabet_size = acts.shape[2]
     minibatch = acts.shape[1]
+
+    # create return variables
+    grads = np.zeros_like(acts, dtype=np.float32)
     cost = np.zeros((minibatch,), dtype=np.float32)
-    libwarpctc.cpu_ctc(acts, grads, flat_labels, label_lengths, sizes, alphabet_size, minibatch, cost, 1)
+
+    # compute
+    libwarpctc.cpu_ctc(acts, grads, labels, label_lens, act_lens, alphabet_size, minibatch, cost, 1)
     return cost, grads
 
 if __name__ == '__main__':
     acts = np.array([[[0.1, 0.6, 0.1, 0.1, 0.1]],
                      [[0.1, 0.1, 0.6, 0.1, 0.1]]])
     assert acts.shape == (2, 1, 5) # max_len is 2, minibatch is 1, alphabet_size is 5
-    labels = [np.array([1, 2])]
+    labels = np.array([1, 2])
+    label_lens = np.array([2])
     sizes = np.array([2])
-    cost, grads = cpu_ctc(acts, labels, sizes)
+    cost, grads = cpu_ctc(acts, sizes, labels, label_lens)
     print "cost:", cost.sum()
     print "expected cost:", 2.46285844
 
